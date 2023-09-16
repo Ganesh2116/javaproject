faa19cb959e6cf27bdb3cb2a5f12a16013e15e15
ganesh2116 <ganeshkundlikbharti728@gmail.com>
2023-09-17 03:55:48 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
